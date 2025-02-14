a85a8d287d9333b1694e2f3f7a5a22c0df588651
pavel1111pavel <seminarist@yandex.ru>
2025-02-14 20:43:48 +0300

    попытка завести laragon

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Обо мне</title>
+    <link type="image/x-icon" rel="shortcut icon"
+        href="https://www.ispring.ru/ispring_content/content/images/favicon/favicon.ico">
+</head>
+
+<body>
+    <header class="site_header">
+        <a href="/post" class="post page">Страница Post</a>
+        <nav class="nav"></nav>
+    </header>
+
+    <main>
+        <h1>⚡Никитин Павел</h1>
+        <img src="https://sun7-21.userapi.com/impg/oXxAB8sFO44BftjpE8CTcxVDe_IhrsfaX2HPBA/bymwVKa0_10.jpg?size=1439x2160&quality=95&sign=0773b1e81bd634192c103842e8f1a06c&type=album"
+            height="200px" alt="я и воджь" border="10 px" vspace="2px">
+        <h2>⚡ИПС 2024-2026</h2>
+        <h2>⚡О себе</h2>
+    </main>
+
+    <footer>
+        <div id="footer">
+            <a id="social_link" href="https://vk.com/pavelnikitin1985">VK</a>
+            <a id="social_link" href="https://t.me/nikitin_pa">Telegram</a>
+            <a id="social_link" href="mailto:seminarist@yandex.ru">Email</a>
+        </div>
+    </footer>
+</body>
